a85e1e9203c989c8766979120e2e2a8910fe45fd
AJ Moon <alex.jared.moon@gmail.com>
2016-12-14 14:30:06 -0800

    Initial code introduction

---
+++ main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+	"os"
+	"strings"
+  "strconv"
+)
+
+// Helper function to pull the href attribute from a Token
+func getHref(t html.Token) (ok bool, href string) {
+	// Iterate over all of the Token's attributes until we find an "href"
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			ok = true
+		}
+	}
+
+	// "bare" return will return the variables (ok, href) as defined in
+	// the function definition
+	return
+}
+
+// Extract all http** links from a given webpage
+func crawl(url string, ch chan string, chFinished chan bool) {
+  fmt.Println("Attempting to Crawl: \"" + url + "\"")
+
+	resp, err := http.Get(url)
+
+	defer func() {
+		// Notify that we're done after this function
+		chFinished <- true
+	}()
+
+	if err != nil {
+		fmt.Println("ERROR: Failed to crawl \"" + url + "\"")
+    fmt.Println("ERROR: " + strconv.Itoa(resp.StatusCode))
+		return
+	}
+
+	b := resp.Body
+	defer b.Close() // close Body when the function returns
+
+	z := html.NewTokenizer(b)
+
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			// End of the document, we're done
+			return
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			// Check if the token is an <a> tag
+			isAnchor := t.Data == "a"
+			if !isAnchor {
+				continue
+			}
+
+			// Extract the href value, if there is one
+			ok, href := getHref(t)
+			if !ok {
+				continue
+			}
+
+			// Make sure the url begines in http**
+
+      switch strings.Split(href, ":")[0] {
+        case "mailto", "http", "https", "ftp" :
+            /*if href == url {
+              continue
+            }*/
+            ch <- href
+        default :
+          if strings.LastIndex(url, "/") != len(url)-1 && strings.Index(href, "/") != 0 {
+            url = url + "/"
+          }else if strings.LastIndex(url, "/") == len(url)-1 && strings.Index(href, "/") == 0  {
+            url = strings.TrimSuffix(url, "/")
+          }
+          ch <- (url + href)
+      }
+
+
+			/*hasProto := strings.Index(href, "http") == 0
+			if hasProto {
+				ch <- href
+			}else if len(href) > 1 && strings.Index(href, "mailto") != 0 {
+        //fmt.Println("url2: " + url2)
+        if strings.LastIndex(url, "/") != len(url)-1 && strings.Index(href, "/") != 0 {
+          url = url + "/"
+        }
+        ch <- (url + href)
+      }else{
+        ch <- href
+      }*/
+		}
+	}
+}
+
+func main() {
+	foundUrls := make(map[string]bool)
+	seedUrls := os.Args[1:]
+
+	// Channels
+	chUrls := make(chan string)
+	chFinished := make(chan bool)
+
+	// Kick off the crawl process (concurrently)
+	for _, url := range seedUrls {
+		go crawl(url, chUrls, chFinished)
+	}
+
+	// Subscribe to both channels
+	for c := 0; c < len(seedUrls); {
+		select {
+  		case url := <-chUrls:
+  			foundUrls[url] = true
+        //crawl(url, chUrls, chFinished)
+  		case <-chFinished:
+  			c++
+		}
+	}
+
+
+
+	// We're done! Print the results...
+
+	fmt.Println("\nFound", len(foundUrls), "unique urls:\n")
+
+	for url, _ := range foundUrls {
+		fmt.Println(" - " + url)
+	}
+
+	close(chUrls)
+}
